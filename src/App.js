06e43a3a994f083d11f946cfba53e82e3f6910e2
Emanoel <carmoemanoel@gmail.com>
2024-05-14 22:05:33 -0300

    feat: created react course repository

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css"
+
+function App() {
+  return (
+    <body>
+      <div className="card">
+      <h1>React Course</h1>
+      <p>Front-End Javascript</p>
+    </div>
+    </body>
+    
+  );
+}
+
+export default App;
