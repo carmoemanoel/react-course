8b58cb35f1e91fffb67ab9e3f989b7de41798ff4
Emanoel <brunomirands237@gmail.com>
2024-06-04 20:34:31 -0300

    feat: created components style module css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import '../src/App.css'
 
-import Square from './styles/styled-component/components/Square';
+// import Square from './styles/styled-component/components/Square';
+import Headline from './styles/css-modules/components/Headline';
 
 
 
 function App() {
   return (
     <body>
-       <Square borderStyle ='dotted'/>
+       {/* <Square borderStyle ='dotted'/> */}
+       <Headline/>
     </body>
   );
 }
