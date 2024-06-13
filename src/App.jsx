7244a0261f81187e5f9ffb26826282f8af5a384c
Emanoel <carmoemanoel@gmail.com>
2024-06-12 21:32:14 -0300

    fix: Alterando page Team

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { Router } from 'react-router-dom';
+// import { Router } from 'react-router-dom';
 import '../src/App.css'
+import Navbar from './hooks/components/Navbar';
 
 // import Accordion from './hooks/components/accodion';
 
@@ -8,9 +9,7 @@ import '../src/App.css'
 function App() {
   return (
     <body>
-      <Router>
-        
-      </Router>
+      <Navbar/>
       {/* <Accordion/> */}
     </body>
   );
--- src/components/team/index.jsx
+++ src/components/team/index.jsx
@@ -1,26 +1,110 @@
-import "./style.css"
+import "./styles.css"
 import { Team } from "./src/pattern/composition"
 
-
-const mock = [
-    1, 2, 3, 4, 5, 6
+const TeamUsers = [
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5two6GvgUnTKCvY5MFEQyCBIvBtKLY7l59Q&s",
+        name: "Emanoel Carmo",
+        role: "Programador",
+        description: "THE KING",
+        mediaSocial: {
+            linkedin: "https://br.linkedin.com/in/lucascorreaa/",
+            x: "https://x.com/",
+            dribble: "https://dribbble.com/"    
+        } 
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5two6GvgUnTKCvY5MFEQyCBIvBtKLY7l59Q&s",
+        name: "Emanoel Carmo",
+        role: "Programador",
+        description: "THE KING",
+        mediaSocial: {
+            linkedin: "https://br.linkedin.com/in/lucascorreaa/",
+            x: "https://x.com/",
+            dribble: "https://dribbble.com/"    
+        } 
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5two6GvgUnTKCvY5MFEQyCBIvBtKLY7l59Q&s",
+        name: "Emanoel Carmo",
+        role: "Programador",
+        description: "THE KING",
+        mediaSocial: {
+            linkedin: "https://br.linkedin.com/in/lucascorreaa/",
+            x: "https://x.com/",
+            dribble: "https://dribbble.com/"    
+        } 
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5two6GvgUnTKCvY5MFEQyCBIvBtKLY7l59Q&s",
+        name: "Emanoel Carmo",
+        role: "Programador",
+        description: "THE KING",
+        mediaSocial: {
+            linkedin: "https://br.linkedin.com/in/lucascorreaa/",
+            x: "https://x.com/",
+            dribble: "https://dribbble.com/"    
+        } 
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5two6GvgUnTKCvY5MFEQyCBIvBtKLY7l59Q&s",
+        name: "Emanoel Carmo",
+        role: "Programador",
+        description: "THE KING",
+        mediaSocial: {
+            linkedin: "https://br.linkedin.com/in/lucascorreaa/",
+            x: "https://x.com/",
+            dribble: "https://dribbble.com/"    
+        } 
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5two6GvgUnTKCvY5MFEQyCBIvBtKLY7l59Q&s",
+        name: "Emanoel Carmo",
+        role: "Programador",
+        description: "THE KING",
+        mediaSocial: {
+            linkedin: "https://br.linkedin.com/in/lucascorreaa/",
+            x: "https://x.com/",
+            dribble: "https://dribbble.com/"    
+        } 
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5two6GvgUnTKCvY5MFEQyCBIvBtKLY7l59Q&s",
+        name: "Emanoel Carmo",
+        role: "Programador",
+        description: "THE KING",
+        mediaSocial: {
+            linkedin: "https://br.linkedin.com/in/lucascorreaa/",
+            x: "https://x.com/",
+            dribble: "https://dribbble.com/"    
+        } 
+    },
 ]
 
 export default function TeamComposition() {
     return (
         <div className="team-container">
+        <Team.Section />
+        <div className="team-content-wrapper">
 
-            <Team.Section />
-
-            {mock.map((element) => {
-                return <Team.Content key={element} />
-            
-            })}
-
-            <Team.Content />
-            
-            <Team.Main />
-           
+            {
+            TeamUsers.map((user, key) => {
+                return (
+                    <Team.Content 
+                        key={key}
+                        image={user.image}
+                        name={user.name}
+                        role={user.role}
+                        description={user.description}
+                        linkedin={user.mediaSocial.linkedin}
+                        x={user.mediaSocial.x}
+                        dribble={user.mediaSocial.dribble}
+                    />
+                )
+            })
+        }
+        </div>
+        <Team.Main />
         </div>
     )
 }
