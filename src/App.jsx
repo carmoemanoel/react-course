026ef9273fe55143ffce05e10b015d304bf67e19
Emanoel <carmoemanoel@gmail.com>
2024-06-12 22:04:33 -0300

    feat: Criando novas pages com rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 // import { Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import '../src/App.css'
-import Navbar from './hooks/components/Navbar';
+import PageOne from './hooks/pages/PageOne/pageOne';
+// import Navbar from './hooks/components/Navbar';
 
 // import Accordion from './hooks/components/accodion';
 
@@ -9,7 +11,10 @@ import Navbar from './hooks/components/Navbar';
 function App() {
   return (
     <body>
-      <Navbar/>
+      <Routes>
+        <Route path="/" component={PageOne} />
+      </Routes>
+      {/* <Navbar/> */}
       {/* <Accordion/> */}
     </body>
   );
